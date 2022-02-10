a060465ba54b97519c3b1244862049697297742e
Robert Sainton <robertsainton@outlook.com>
2022-02-10 17:25:15 -0500

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+    const num1 = 31 ;
+    const num2 = 2 ;
+    const multiply = num2 * num1;
+
+    const random = Math.floor(Math.random() + 1) ;
+    
+    const max = Math.max (5, 10, 15, 20);
+
+    const num3 = 40;
+    const num4 = 6;    
+    const mod = num3 % num4;
